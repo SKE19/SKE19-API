03d6cbd8881d588da0bc4e4e80aff2db60cc4edd
GToidZ <amazijapan@gmail.com>
2021-09-17 12:15:11 +0700

    Basic CSV parsing to JSON.

---
+++ src/data/README.md
@@ -0,0 +1,7 @@
+# Data Structure
+The data is a CSV-file with the following columns, you should be able to provide data by yourself.
+```
+email, studentId, nameTH, nameEN, nickTH, nickEN, instagram
+```
+
+**Notice: The author nor the collaborator(s) will give the real data to those who are not in [SKE19](https://github.com/SKE19)**
--- src/server.js
+++ src/server.js
@@ -1,10 +1,62 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const server = express()
+const fs = require('fs')
+const csv = require('csv-parser')
+const http = require('http')
+const { json } = require('express')
+const app = express()
 
-server.use(cors())
+let skeData = { students : {} }
 
-server.listen(process.env.PORT, () => {
-    console.log(`Server started at port ${process.env.PORT}`)
+function readCsv(filepath){
+    return new Promise((resolve, reject)=>{
+        fs.createReadStream(filepath)
+        .pipe(csv())
+        .on('data', (row) => {
+            skeData['students'][row['studentId']] = {
+                firstNameEN: row['nameEN'].split(" ")[0],
+                lastNameEN: row['nameEN'].split(" ")[1],
+                nickEN: row['nickEN'],
+                firstNameTH: row['nameTH'].split(" ")[0],
+                lastNameTH: row['nameTH'].split(" ")[1],
+                nickTH: row['nickTH'],
+                email: row['email'],
+                instagram: '@' + row['instagram']
+            }
+        })
+        .on('end', () => {
+            resolve()
+        })
+        .on('error', reject)
+    })
+}
+
+app.use(cors())
+app.use(json())
+
+app.use('/', (req, res) => {
+    res.json(skeData)
 })
+
+readCsv(process.env.DATA_PATH).then(() => {
+    
+    const server = http.createServer(app)
+
+    server.listen(process.env.PORT, () => {
+        console.log(skeData)
+        console.log(`Server started at port ${process.env.PORT}`)
+    })
+
+})
+
+/* exports = Object.freeze({
+    server: http.createServer(app)
+    init: () => {
+        return readCsv(process.env.DATA_PATH).then(() => {
+            this.
+            })
+        })
+    }
+})
+*/
